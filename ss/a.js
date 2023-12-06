ba9550a05c427e235bb6fd61b2eeadfe2abaf339
chandrasekhar432 <chandrasekhar432.j@gmail.com>
2023-12-06 13:50:43 +0530

    this commit is for tagging

---
+++ ss/a.js
@@ -0,0 +1,48 @@
+// var fs  = require('fs');
+// var filedata ='"hii';
+
+// fs,fs.readFile('Demo.txt','utf8',function (err,data){
+//     filedata = data;
+// })
+
+// console.log(filedata);
+
+
+
+
+// let obj = [
+    
+//     [
+//         {
+//             "Subject_Name" :"Maths",
+//              "Marks":"100"
+    
+//         },
+//         {
+//             "Subject_Name": "English", 
+//             "Marks" :"60"
+    
+//         }
+
+
+//     ]
+   
+// ]
+
+// console.log(obj [0][1].Subject_Name);
+// // console.log(obj(0).Marks);
+// // console.log(obj[2].Marks);
+
+
+// let textdata = "remove spaces"; 
+// textdata=textdata.toString().replace(/\s{2,}/,""); 
+// console.log(textdata);
+
+
+var c= { greeting: 'Welcome' };
+var d;
+d = c ;
+
+c.greeting ='Welcome to gibots';
+console.log(c);
+console.log(d);
